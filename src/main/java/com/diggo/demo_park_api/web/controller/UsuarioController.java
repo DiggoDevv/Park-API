edd9ccaa5f640d5de355a1a4c0e58f5467de227c
Rodrigo <rodrigobaalves.2004@gmail.com>
2025-07-08 13:12:38 -0300

    create user

---
+++ src/main/java/com/diggo/demo_park_api/web/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package com.diggo.demo_park_api.web.controller;
+
+import com.diggo.demo_park_api.entity.Usuario;
+import com.diggo.demo_park_api.service.UsuarioService;
+import com.diggo.demo_park_api.web.dto.UsuarioCreateDto;
+import com.diggo.demo_park_api.web.dto.UsuarioResponseDto;
+import com.diggo.demo_park_api.web.dto.mapper.UsuarioMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto){
+        Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id){
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario){
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll(){
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(users);
+    }
+}
+
