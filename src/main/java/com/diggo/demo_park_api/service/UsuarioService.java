7052cb8e4e0c822a1ab8fa5bc5b83d221280cde6
Rodrigo <rodrigobaalves.2004@gmail.com>
2025-07-09 13:18:50 -0300

    adding custom exception handling

--- src/main/java/com/diggo/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/diggo/demo_park_api/service/UsuarioService.java
@@ -1,8 +1,12 @@
 package com.diggo.demo_park_api.service;
 
 import com.diggo.demo_park_api.entity.Usuario;
+import com.diggo.demo_park_api.exception.EntityNotFoundException;
+import com.diggo.demo_park_api.exception.PasswordInvalidException;
+import com.diggo.demo_park_api.exception.UsernameUniqueViolationException;
 import com.diggo.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,25 +20,28 @@ public class UsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
-        return usuarioRepository.save(usuario);
+        try {
+            return usuarioRepository.save(usuario);
+        } catch (DataIntegrityViolationException ex){
+            throw new UsernameUniqueViolationException(String.format("Username {%s} já cadastrado", usuario.getUsername()));
+        }
     }
 
     @Transactional(readOnly = true)
     public Usuario buscarPorId(long id) {
         return usuarioRepository.findById(id).orElseThrow(
-                ()-> new RuntimeException("Usuário não encontrado")
-        );
+                ()-> new EntityNotFoundException(String.format("Usuário não encontrado", id)));
     }
 
     @Transactional
     public Usuario editarSenha(Long id, String senhaAtual, String novaSenha, String confirmaSenha) {
         if (!novaSenha.equals(confirmaSenha)){
-            throw new RuntimeException("Nova senha não confere com confirmação de senha");
+            throw new PasswordInvalidException("Nova senha não confere com confirmação de senha");
         }
 
         Usuario user = buscarPorId(id);
         if(!user.getPassword().equals(senhaAtual)){
-            throw new RuntimeException("sua senha não confere");
+            throw new PasswordInvalidException("sua senha não confere");
         }
         user.setPassword(novaSenha);
         return user;
--- src/main/java/com/diggo/demo_park_api/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/diggo/demo_park_api/web/exception/ApiExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.diggo.demo_park_api.web.exception;
 
+import com.diggo.demo_park_api.exception.EntityNotFoundException;
+import com.diggo.demo_park_api.exception.PasswordInvalidException;
+import com.diggo.demo_park_api.exception.UsernameUniqueViolationException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +18,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ApiExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+    @ExceptionHandler(PasswordInvalidException.class)
+    public ResponseEntity<ErrorMessage> passwordInvalidException
+            (RuntimeException ex, HttpServletRequest request) {
+        log.error("Api Error -", ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessage> entityNotFoundException
+            (RuntimeException ex, HttpServletRequest request) {
+        log.error("Api Error -", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+    @ExceptionHandler(UsernameUniqueViolationException.class)
+    public ResponseEntity<ErrorMessage> UniqueViolationException
+            (RuntimeException ex, HttpServletRequest request) {
+        log.error("Api Error -", ex);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.CONFLICT, ex.getMessage()));
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorMessage> methodArgumentNotValidException
